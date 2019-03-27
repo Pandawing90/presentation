13371fe61c109d188c3879a06f67bd51cbff04a7
Pandawing90 <48037860+Pandawing90@users.noreply.github.com>
2019-03-26 20:39:16 -0400

    Add files via upload

---
+++ presentation-master/js/site.js
@@ -0,0 +1,25 @@
+console.log("External file");
+
+var cty;
+var cityFrom = "";
+var cityName = "New York City";
+var x =32;
+
+console.log("cityName, x: ", cityName, x)
+
+var firstname = "Sharell" , lastname = "Wingfield";
+console.log("firstname,lastname", firstname, lastname);
+
+var s = "2019";
+var s2 =parseInt("2019") + s;
+var n = 2001;
+console.log("age: ", s - n);
+console.log("age: ", s + n);
+console.log("age: ", s + parseInt(n) );
+
+
+
+/*
+Assigning a variable to store strings and interger:
+*/
+
